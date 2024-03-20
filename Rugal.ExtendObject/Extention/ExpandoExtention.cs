c2b9919acb98eee2a948a3a1a91428246fdd2b1e
Rugal <john2653589@gmail.com>
2024-03-20 16:23:28 +0800

    Rugal Update

--- Rugal.ExtendObject/Extention/ExpandoExtention.cs
+++ Rugal.ExtendObject/Extention/ExpandoExtention.cs
@@ -4,7 +4,8 @@ namespace Rugal.ExtendObject.Extention
 {
     public static class ExpandoExtention
     {
-        public static IDictionary<string, object> Extend(this object Target, object Source)
+        public static IDictionary<string, object> Extend<TSource>(this object Target, TSource Source)
+            where TSource : class
         {
             var Result = new Dictionary<string, object>()
                 .WithObjectProperty(Target)
@@ -12,7 +13,8 @@ namespace Rugal.ExtendObject.Extention
 
             return Result;
         }
-        public static IDictionary<string, object> WithObjectProperty(this IDictionary<string, object> Map, object Source)
+        public static IDictionary<string, object> WithObjectProperty<TSource>(this IDictionary<string, object> Map, TSource Source)
+            where TSource : class
         {
             foreach (var Item in GetPairs(Source))
             {
@@ -21,7 +23,8 @@ namespace Rugal.ExtendObject.Extention
             }
             return Map;
         }
-        private static IEnumerable<KeyValuePair<string, object>> GetPairs(object Source)
+        private static IEnumerable<KeyValuePair<string, object>> GetPairs<TSource>(TSource Source)
+            where TSource : class
         {
             var Properties = Source.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
             var Result = Properties
